9c622053b716a1b8730e5e1389505e60264226d7
Jarod Nakamoto <jknakamoto@cpp.edu>
2017-11-06 13:20:07 -0800

    Setup

---
+++ Subject.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of interface Subject here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Subject
+{
+    void Attach(Observer o);
+    void Detach(Observer o);
+    void Notify();
+}
---
+++ User.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class User here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class User implements Subject, Observer
+{
+    public User(){
+    
+    }
+    
+    
+    //subject methods
+    public void Attach(Observer o){
+    
+    }
+    public void Detach(Observer o){
+    
+    }
+    public void Notify(){
+    
+    }
+    
+    //observer methods
+    public void update(){
+    
+    }
+}
